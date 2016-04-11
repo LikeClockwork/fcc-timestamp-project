90ad7d570fe8f2c124f343d661a2c188c94ba63d
Jack <jack@likeclockworkdesign.com>
2016-04-11 16:50:05 +0000

    More files added.

---
+++ tsserver.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+
+var app = express();
+
+require('dotenv').load();
+require('./app/config/passport')(passport);
+
+mongoose.connect(process.env.MONGO_URI);
+
+/*
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
+
+app.use(session({
+	secret: 'secretClementine',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+*/
+
+app.use('/',express.static(process.cwd() + '/ts'));
+
+app.get('*', function( req, res) {
+    
+   var dateString = Date.parse(unescape(req.path.substring(1,req.path.length)));
+
+   if(isNaN(dateString)) {
+       dateString = new Date(+unescape(req.path.substring(1,req.path.length)) * 1000);
+   }
+
+   var myDate = {
+           unix : null,
+           natural : null
+   }
+    
+   console.log(+unescape(req.path.substring(1,req.path.length)));
+    
+   if(!isNaN(dateString)) {
+       
+       var months = [
+           'January',
+           'February',
+           'March',
+           'April',
+           'May',
+           'June',
+           'July',
+           'August',
+           'September',
+           'October',
+           'November',
+           'December'
+           ]
+       
+       var date = new Date(dateString);
+       
+       var month = months[date.getMonth()];
+       var day = date.getDate();
+       var year = date.getFullYear();
+       
+       var dateFormatted = month + ' ' + day + ', ' + year;
+       
+       myDate.unix = dateString/1000;
+       myDate.natural = dateFormatted;
+   }
+    
+    
+    res.end(JSON.stringify(myDate));
+      
+   
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
